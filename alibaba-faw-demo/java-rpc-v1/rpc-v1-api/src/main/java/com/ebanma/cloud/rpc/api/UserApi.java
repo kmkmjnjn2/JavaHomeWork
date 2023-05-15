46faf637d1a412caec4ea3ba0098f235818fbb0a
Sawadika <kmkmjnjn2@126.com>
2023-05-15 15:43:49 +0800

    Delete alibaba-faw-demo/java-rpc-v1 directory

--- alibaba-faw-demo/java-rpc-v1/rpc-v1-api/src/main/java/com/ebanma/cloud/rpc/api/UserApi.java
+++
@@ -1,7 +0,0 @@
-package com.ebanma.cloud.rpc.api;
-
-import com.ebanma.cloud.rpc.api.dto.UserInfoDTO;
-
-public interface UserApi {
-    UserInfoDTO getById(Integer id);
-}
--- alibaba-faw-demo/java-rpc-v1/rpc-v1-api/src/main/java/com/ebanma/cloud/rpc/api/dto/UserInfoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ebanma.cloud.rpc.api.dto;
-
-import lombok.Data;
-
-@Data
-public class UserInfoDTO {
-    private Integer id;
-    private String name;
-}
